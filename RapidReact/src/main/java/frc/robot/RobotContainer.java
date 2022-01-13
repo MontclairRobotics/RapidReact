95948b4e3cdc5cec698eb7ed223465a9058c6dde
Dylan Rafael <dylanrafael05@gmail.com>
2022-01-13 16:24:26 -0500

    Moved to new folders

--- RapidReact/src/main/java/frc/robot/RobotContainer.java
+++ RapidReact/src/main/java/frc/robot/RobotContainer.java
@@ -6,8 +6,8 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.DriveCommand;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.drive.DriveCommand;
 import frc.robot.subsystems.DrivetainSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
