362b34d76b339cf8c30e2939deb688f4656b6fc6
Dylan Rafael <dylanrafael05@gmail.com>
2022-01-27 14:11:52 -0500

    Update back to java 17

--- src/main/java/frc/robot/framework/RobotState.java
+++ src/main/java/frc/robot/framework/RobotState.java
@@ -1,27 +1,7 @@
 package frc.robot.framework;
 
-import java.math.RoundingMode;
-
-public class RobotState
+public record RobotState(String id)
 {
-    private String id;
-
-    public RobotState(String id)
-    {
-        this.id = id;
-    }
-
-    @Override
-    public boolean equals(Object obj) 
-    {
-        if(obj instanceof RobotState)
-        {
-            return ((RobotState)obj).id.equals(id);
-        }
-
-        return false;
-    }
-
     // Default members
     public static final RobotState 
         TELEOP = new RobotState("teleop"),
