b7b9f401f892a96db7781b3979269badbb8961d7
AwesomeJedi17 <97196492+Abraham17-K@users.noreply.github.com>
2022-02-12 14:54:05 +0000

    Added Color 1 Patterns

--- src/main/java/frc/robot/rev/BlinkinLEDMode.java
+++ src/main/java/frc/robot/rev/BlinkinLEDMode.java
@@ -9,7 +9,19 @@ public enum BlinkinLEDMode
     // LILA: fixed palette pattern (page 2)
 
     // ABE: color 1 patterns
+    C1_END_TO_END_BLEND_TO_BLACK(-0.03),
+    C1_LARSON_SCANNER(-0.01),
+    C1_LIGHT_CHASE(0.01),
 
+    C1_HEARTBEAT_SLOW(0.03),
+    C1_HEARTBEAT_MEDIUM(0.05),
+    C1_HEARTBEAT_FAST(0.07),
+
+    C1_BREATH_SLOW(0.09),
+    C1_BREATH_FAST(0.11),
+    
+    C1_SHOT(0.13),
+    C1_STROBE(0.15),
     // MERT: color 2 patterns
 
     // CAITIE: color 1 & 2 patterns
