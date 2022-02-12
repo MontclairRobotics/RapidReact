6beadd5f840e7e4d892943c5f2ff2008644a7ef7
Dylan <dylanrafael05@gmail.com>
2022-02-12 09:59:43 -0500

    inputted blinkin data (caitie)

--- src/main/java/frc/robot/rev/BlinkinLEDMode.java
+++ src/main/java/frc/robot/rev/BlinkinLEDMode.java
@@ -13,6 +13,16 @@ public enum BlinkinLEDMode
     // MERT: color 2 patterns
 
     // CAITIE: color 1 & 2 patterns
+    SPARKLE_C1_ON_C2(0.37),
+    SPARKLE_C2_ON_C1(0.39),
+    COLOR_GRADIENT_C1_AND_C2(0.41),
+    BPM_C1_AND_C2(0.43),
+    END_TO_END_BLEND_C1_TO_C2(0.45),
+    END_TO_END_BLEND(0.47),
+    __SETUP__C1_AND_C2_NO_BLENDING(0.49),
+    TWINKLES_C1_AND_C2(0.51),
+    COLOR_WAVES_C1_AND_C2(0.53),
+    SINELON_C1_AND_C2(0.55),
 
     // MAX: solid color patterns
 
