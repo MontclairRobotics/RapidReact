a0a9223c61d1858aee028c54022959892b4a3d65
Dylan Rafael <dylanrafael05@gmail.com>
2022-02-12 09:58:15 -0500

    entered data for Blinkin LED (Dylan)

--- src/main/java/frc/robot/rev/BlinkinLEDMode.java
+++ src/main/java/frc/robot/rev/BlinkinLEDMode.java
@@ -5,6 +5,30 @@ public enum BlinkinLEDMode
     // DYLAN: fixed palette pattern (first page)
     RAINBOW_RAINBOW(-0.99),
     RAINBOW_PARTY(-0.97),
+    RAINBOW_OCEAN(-0.95),
+    RAINBOW_LAVA(-0.93),
+    RAINBOW_FOREST(-0.91),
+    RAINBOW_GLITTER(-0.89),
+
+    CONFETTI(-0.87),
+    
+    SHOT_RED(-0.85),
+    SHOT_BLUE(-0.83),
+    SHOT_WHITE(-0.81),
+
+    SINELON_RAINBOW(-0.79),
+    SINELON_PARTY(-0.77),
+    SINELON_OCEAN(-0.75),
+    SINELON_LAVA(-0.73),
+    SINELON_FOREST(-0.71),
+
+    BPM_RAINBOW(-0.69),
+    BPM_PARTY(-0.67),
+    BPM_OCEAN(-0.65),
+    BPM_LAVA(-0.63),
+    BPM_FOREST(-0.61),
+    
+    FIRE_MEDIUM(-0.59),
 
     // LILA: fixed palette pattern (page 2)
 
