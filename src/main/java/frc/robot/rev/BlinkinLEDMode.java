842c984299b643a83bf8171194895d6a501df800
Mert <mertkiran06@gmail.com>
2022-02-12 09:57:28 -0500

    entered data for Blinkin LED

--- src/main/java/frc/robot/rev/BlinkinLEDMode.java
+++ src/main/java/frc/robot/rev/BlinkinLEDMode.java
@@ -11,6 +11,16 @@ public enum BlinkinLEDMode
     // ABE: color 1 patterns
 
     // MERT: color 2 patterns
+    C2_END_TO_END_BLEND_TO_BLACK(.17),
+    C2_LARSON_SCANNER(.19),
+    C2_LIGHT_CHASE(.21),
+    C2_HEARTBEAT_SLOW(.23),
+    C2_HEARTBEAT_MEDIUM(.25),
+    C2_HEARTBEAT_FAST(.27),
+    C2_BREATH_SLOW(.29),
+    C2_BREATH_FAST(.31),
+    C2_SHOT(.33),
+    C2_STROBE(.35),
 
     // CAITIE: color 1 & 2 patterns
 
